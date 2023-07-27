d3441d202a31d1efc5a6cf96e968eb5b7ad7eb96
dylanblokhuis <dylanblokhuis3@hotmail.com>
2023-07-27 09:33:08 +0200

    1.1.5

--- instantcss.php
+++ instantcss.php
@@ -3,14 +3,14 @@
 /**
  * Plugin Name: Instant CSS
  * Description: Write your styles beautifully with the power of Visual Studio Code
- * Version:     1.1.4
+ * Version:     1.1.5
  * Author:      Dylan Blokhuis
  * Author URI:  https://github.com/dylanblokhuis
  * License:     GPL-3.0
  * License URI: https://raw.githubusercontent.com/dylanblokhuis/instantcss-wp/master/LICENSE
  */
 
-define('ICSS_VERSION', '1.1.4');
+define('ICSS_VERSION', '1.1.5');
 
 if ( ! defined( 'ABSPATH' ) ) {
 	die( 'Access denied.' );
